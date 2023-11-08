cd912fb46d924849fcb35d6fe04179fd62e63917
fqwert <yanglv2@huawei.com>
2023-11-08 10:55:48 +0800

    rust代码添加Cargo.toml
    Signed-off-by: fqwert <yanglv2@huawei.com>
    Change-Id: Ibbb6da415dd3d9dff37f4ada3c0916d623560b43

--- interfaces/rust/src/sys_event.rs
+++ interfaces/rust/src/sys_event.rs
@@ -1,19 +1,17 @@
-/*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2023 Huawei Device Co., Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-use std::ffi::{CString, c_char, c_int, c_uint, c_ulonglong};
+use std::ffi::{c_char, c_int, c_uint, c_ulonglong, CString};
 
 /// Length limit for the name of a HiSysEventParam.
 const MAX_LENGTH_OF_PARAM_NAME: usize = 49;
@@ -180,7 +178,7 @@ pub fn parse_type_len<T>(_: T) -> (&'static str, usize) {
     let mut bytes = value_type.as_bytes();
     if bytes[0] == b'&' {
         value_type = &value_type[1..];
-		bytes = value_type.as_bytes();
+        bytes = value_type.as_bytes();
     }
     if bytes[0] != b'[' {
         // not a array
@@ -203,7 +201,9 @@ pub fn parse_type_len<T>(_: T) -> (&'static str, usize) {
             break;
         }
     }
-    let array_len = value_type[len_start..(bytes.len() - 1)].parse::<usize>().unwrap();
+    let array_len = value_type[len_start..(bytes.len() - 1)]
+        .parse::<usize>()
+        .unwrap();
     if bytes[1] == b'&' {
         // reference
         (&value_type[2..val_end], array_len)
@@ -224,14 +224,20 @@ pub fn build_string_arrays<'a>(param_name: &'a str, str_arr: &[&'a str]) -> HiSy
         param_name,
         param_type: HiSysEventParamType::ParamTypeStringArray,
         param_value: HiSysEventParamValue {
-            void_ptr_: std::boxed::Box::<[*const c_char]>::into_raw(dest.into_boxed_slice()) as *const c_int as *const (),
+            void_ptr_: std::boxed::Box::<[*const c_char]>::into_raw(dest.into_boxed_slice())
+                as *const c_int as *const (),
         },
         array_size: str_arr.len(),
     }
 }
 
 /// Write system event.
-pub(crate) fn write(event_domain: &str, event_name: &str, event_type: c_int, event_params: &[HiSysEventParam]) -> i32 {
+pub(crate) fn write(
+    event_domain: &str,
+    event_name: &str,
+    event_type: c_int,
+    event_params: &[HiSysEventParam],
+) -> i32 {
     let mut params_wrapper: Vec<HiSysEventParamWrapper> = vec![];
     for i in 0..event_params.len() {
         params_wrapper.push(HiSysEventParamWrapper {
@@ -240,7 +246,10 @@ pub(crate) fn write(event_domain: &str, event_name: &str, event_type: c_int, eve
             param_value: event_params[i].param_value,
             array_size: event_params[i].array_size as c_uint,
         });
-        crate::utils::trans_slice_to_array(event_params[i].param_name, &mut params_wrapper[i].param_name);
+        crate::utils::trans_slice_to_array(
+            event_params[i].param_name,
+            &mut params_wrapper[i].param_name,
+        );
     }
     let func = CString::new(crate::function!()).expect("Need a valid function name");
     let domain = CString::new(event_domain).expect("Need a valid domain name");
@@ -254,14 +263,20 @@ pub(crate) fn write(event_domain: &str, event_name: &str, event_type: c_int, eve
             event_name.as_ptr() as *const c_char,
             event_type,
             params_wrapper.as_mut_ptr(),
-            event_params.len() as c_uint
+            event_params.len() as c_uint,
         )
     }
 }
 
 extern "C" {
     /// ffi border function.
-    fn HiSysEventWriteWrapper(func: *const c_char, line: c_ulonglong, domain: *const c_char,
-        name: *const c_char, event_type: c_int, params: *const HiSysEventParamWrapper,
-        size: c_uint) -> c_int;
+    fn HiSysEventWriteWrapper(
+        func: *const c_char,
+        line: c_ulonglong,
+        domain: *const c_char,
+        name: *const c_char,
+        event_type: c_int,
+        params: *const HiSysEventParamWrapper,
+        size: c_uint,
+    ) -> c_int;
 }
--- test/unittest/rust/src/main.rs
+++ test/unittest/rust/src/main.rs
@@ -1,22 +1,21 @@
-/*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2023 Huawei Device Co., Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 extern crate hisysevent;
 
-use hisysevent::{EventType, HiSysEventRecord, RuleType, Watcher, WatchRule};
-use hisysevent::{QueryArg, QueryRule, Querier};
+use hisysevent::{
+    EventType, HiSysEventRecord, Querier, QueryArg, QueryRule, RuleType, WatchRule, Watcher,
+};
 
 const SUCCEED: i32 = 0;
 const LISTENER_NOT_EXIST: i32 = -10;
@@ -28,14 +27,16 @@ fn test_hisysevent_write_001() {
         "HIVIEWDFX",
         "PLUGIN_LOAD",
         EventType::Behavior,
-        &[hisysevent::build_number_param!("INT32_SINGLE", 100i32),
+        &[
+            hisysevent::build_number_param!("INT32_SINGLE", 100i32),
             hisysevent::build_str_param!("STRING_SINGLE", "test_hisysevent_write_001"),
             hisysevent::build_bool_param!("BOOL_SINGLE", true),
             hisysevent::build_number_param!("FLOAT_SINGLE", 4.03f32),
             hisysevent::build_string_array_params!("STRING_ARRAY", &["STRING1", "STRING2"]),
             hisysevent::build_array_params!("INT32_ARRAY", &[8i32, 9i32]),
             hisysevent::build_array_params!("BOOL_ARRAY", &[true, false, true]),
-            hisysevent::build_array_params!("FLOAT_ARRAY", &[1.55f32, 2.33f32, 4.88f32])]
+            hisysevent::build_array_params!("FLOAT_ARRAY", &[1.55f32, 2.33f32, 4.88f32]),
+        ],
     );
     assert!(ret == SUCCEED);
 }
@@ -56,14 +57,18 @@ fn test_hisysevent_add_remove_watcher_001() {
             tag: "",
             rule_type: RuleType::WholeWord,
             event_type: EventType::Behavior,
-        }
+        },
     ];
     // step1: construct a mut watcher.
-    let watcher = Watcher::new(|record: HiSysEventRecord| {
-        assert!(record.get_domain() == "HIVIEWDFX");
-    }, || {
-        // do nothing.
-    }).expect("Construct a watcher by Watcher::new");
+    let watcher = Watcher::new(
+        |record: HiSysEventRecord| {
+            assert!(record.get_domain() == "HIVIEWDFX");
+        },
+        || {
+            // do nothing.
+        },
+    )
+    .expect("Construct a watcher by Watcher::new");
     // step2: add this watcher.
     let mut ret = hisysevent::add_watcher(&watcher, &watch_rules);
     assert!(ret == SUCCEED);
@@ -71,14 +76,20 @@ fn test_hisysevent_add_remove_watcher_001() {
         "HIVIEWDFX",
         "PLUGIN_LOAD",
         EventType::Behavior,
-        &[hisysevent::build_str_param!("STRING_SINGLE", "test_hisysevent_add_remove_watcher_001")]
+        &[hisysevent::build_str_param!(
+            "STRING_SINGLE",
+            "test_hisysevent_add_remove_watcher_001"
+        )],
     );
     assert!(ret == SUCCEED);
     ret = hisysevent::write(
         "HIVIEWDFX",
         "PLUGIN_UNLOAD",
         EventType::Behavior,
-        &[hisysevent::build_str_param!("STRING_SINGLE", "test_hisysevent_add_remove_watcher_001")]
+        &[hisysevent::build_str_param!(
+            "STRING_SINGLE",
+            "test_hisysevent_add_remove_watcher_001"
+        )],
     );
     assert!(ret == SUCCEED);
     // step3: remove this watcher.
@@ -87,7 +98,7 @@ fn test_hisysevent_add_remove_watcher_001() {
     // step4: recycle allocated memories of this watcher.
     watcher.try_to_recycle();
     ret = hisysevent::add_watcher(&watcher, &watch_rules);
-        assert!(ret == LISTENER_NOT_EXIST);
+    assert!(ret == LISTENER_NOT_EXIST);
 }
 
 #[test]
@@ -97,14 +108,20 @@ fn test_hisysevent_query_001() {
         "HIVIEWDFX",
         "PLUGIN_LOAD",
         EventType::Behavior,
-        &[hisysevent::build_str_param!("STRING_SINGLE", "test_hisysevent_query_001")]
+        &[hisysevent::build_str_param!(
+            "STRING_SINGLE",
+            "test_hisysevent_query_001"
+        )],
     );
     assert!(ret == SUCCEED);
     ret = hisysevent::write(
         "HIVIEWDFX",
         "PLUGIN_UNLOAD",
         EventType::Behavior,
-        &[hisysevent::build_str_param!("STRING_SINGLE", "test_hisysevent_query_001")]
+        &[hisysevent::build_str_param!(
+            "STRING_SINGLE",
+            "test_hisysevent_query_001"
+        )],
     );
     assert!(ret == SUCCEED);
     // query event.
@@ -116,35 +133,34 @@ fn test_hisysevent_query_001() {
     let query_rules = [
         QueryRule {
             domain: "HIVIEWDFX",
-            event_list: vec![
-                "PLUGIN_LOAD",
-                "PLUGIN_UNLOAD",
-            ],
+            event_list: vec!["PLUGIN_LOAD", "PLUGIN_UNLOAD"],
             condition: "{\"version\":\"V1\",\"condition\":{\"and\":[{\"param\":\"
                 NAME\",\"op\":\"=\",\"value\":\"SysEventService\"}]}}",
         },
         QueryRule {
             domain: "HIVIEWDFX",
-            event_list: vec![
-                "PLUGIN_LOAD",
-            ],
+            event_list: vec!["PLUGIN_LOAD"],
             condition: "",
-        }
+        },
     ];
     // step1: construct a querier.
-    let querier = Querier::new(|records: &[HiSysEventRecord]| {
-        for item in records {
-            assert!(item.get_domain() == "HIVIEWDFX");
-        }
-    }, |reason: i32, total: i32| {
-        assert!(reason == SUCCEED);
-        assert!(total == QUERY_CNT);
-    }).expect("Construct a querier by Querier::new");
+    let querier = Querier::new(
+        |records: &[HiSysEventRecord]| {
+            for item in records {
+                assert!(item.get_domain() == "HIVIEWDFX");
+            }
+        },
+        |reason: i32, total: i32| {
+            assert!(reason == SUCCEED);
+            assert!(total == QUERY_CNT);
+        },
+    )
+    .expect("Construct a querier by Querier::new");
     // step2: query.
     ret = hisysevent::query(&query_arg, &query_rules, &querier);
     assert!(ret == SUCCEED);
     // step3: recycle allocated memories of this Querier.
     querier.try_to_recycle();
     ret = hisysevent::query(&query_arg, &query_rules, &querier);
-        assert!(ret == LISTENER_NOT_EXIST);
+    assert!(ret == LISTENER_NOT_EXIST);
 }
