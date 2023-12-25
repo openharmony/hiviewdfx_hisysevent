2547f159b57556bd504cefbb6872fe99fd374ca2
xuanfeng <lifeng54@huawei.com>
2023-12-25 10:15:19 +0800

    IssueNo:https://gitee.com/openharmony/hiviewdfx_hisysevent/issues/I8QYT7 Description:添加实况开放能力-实况窗功能hisysevent事件domain Signed-off-by: xuanfeng8 <lifeng54@huawei.com>
    Signed-off-by: xuanfeng <lifeng54@huawei.com>

--- interfaces/native/innerkits/hisysevent/include/hisysevent.h
+++ interfaces/native/innerkits/hisysevent/include/hisysevent.h
@@ -125,6 +125,7 @@ public:
         static constexpr char INTELLI_TV[] = "INTELLITV";
         static constexpr char ISHARE[] = "ISHARE";
         static constexpr char IVI_HARDWARE[] = "IVIHARDWARE";
+        static constexpr char LIVEVIEW_MANAGER[] = "LIVEVIEW_MANAGER";
         static constexpr char LOCATION[] = "LOCATION";
         static constexpr char MEDICAL_SENSOR[] = "MEDICAL_SENSOR";
         static constexpr char MISCDEVICE[] = "MISCDEVICE";
