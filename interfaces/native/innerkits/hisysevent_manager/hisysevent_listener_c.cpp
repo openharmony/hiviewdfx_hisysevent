8e1bf7c9dbfed738785cd3f4bf341ee7407252c3
xuyong <xuyong59@huawei.com>
2025-01-14 09:47:37 +0800

    sync codes to 5.0.2-release
    Signed-off-by: xuyong <xuyong59@huawei.com>

--- interfaces/native/innerkits/hisysevent_manager/hisysevent_listener_c.cpp
+++ interfaces/native/innerkits/hisysevent_manager/hisysevent_listener_c.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -37,6 +37,7 @@ void HiSysEventListenerC::OnEvent(std::shared_ptr<OHOS::HiviewDFX::HiSysEventRec
     HiSysEventRecordC record = {};
     HiSysEventRecordConvertor::ConvertRecord(*sysEvent, record);
     onEvent_(record);
+    HiSysEventRecordConvertor::DeleteRecord(record);
 }
 
 void HiSysEventListenerC::OnServiceDied()
